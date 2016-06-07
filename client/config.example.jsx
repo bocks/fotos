a4f0f032f296ff68bea5e064d50c5736fb1feda7
Daniel Fiore <taptapdan@gmail.com>
2016-06-07 15:57:46 -0700

    Add instructions to config.example

--- client/config.example.jsx
+++ client/config.example.jsx
@@ -1,3 +1,6 @@
+// make your own copy of this file with the name `config.jsx`
+// and then update the variables for your local dev environment
+
 let Config = {
   FACEBOOK_APP_ID: '0000000000000000'
 };
